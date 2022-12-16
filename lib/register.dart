0e2a1aaed7105f51de06a81ae07be340ace87dd2
ardhanurazizah <naardha0@gmail.com>
2022-12-17 00:45:18 +0700

    Halaman Register

--- lib/register.dart
+++ lib/register.dart
@@ -1,135 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:final_project/login.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Register extends StatefulWidget {
+  const Register({super.key});
+  @override
+  State<Register> createState() => _Register();
+}
 
-  // This widget is the root of your application.
+class _Register extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Login'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            'Registration',
-            style: TextStyle(
-                fontSize: 35, color: Colors.teal, fontWeight: FontWeight.bold),
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text("Register Page"),
+            backgroundColor: const Color(0xFF0000ff),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 30),
-            child: Form(
-                child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        hintText: 'Enter Email',
-                        prefixIcon: new Icon(Icons.email),
-                        border: OutlineInputBorder(),
-                      ),
-                      onChanged: (String value) {},
-                      validator: (value) {
-                        return value!.isEmpty ? 'Please enter email' : null;
-                      }),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15),
-                  child: TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        hintText: 'Enter Password',
-                        prefixIcon: new Icon(Icons.password),
-                        border: OutlineInputBorder(),
-                      ),
-                      onChanged: (String value) {},
-                      validator: (value) {
-                        return value!.isEmpty ? 'Please enter password' : null;
-                      }),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 35),
-                  child: MaterialButton(
-                    minWidth: double.infinity,
-                    onPressed: () {},
-                    child: Text('Login'),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Registration',
+                style: TextStyle(
+                    fontSize: 35,
                     color: Colors.teal,
-                    textColor: Colors.white,
-                  ),
+                    fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  hintText: 'Enter Username',
+                  prefixIcon: new Icon(Icons.email),
+                  border: OutlineInputBorder(),
                 ),
-              ],
-            )),
-          )
-        ],
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+                onChanged: (String value) {},
+                validator: ((value) {
+                  return value!.isEmpty ? 'Please Enter Username' : null;
+                }),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Enter Email',
+                  prefixIcon: new Icon(Icons.email),
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (String value) {},
+                validator: ((value) {
+                  return value!.isEmpty ? 'Please Enter Email' : null;
+                }),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter Password',
+                  prefixIcon: new Icon(Icons.password),
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (String value) {},
+                validator: ((value) {
+                  return value!.isEmpty ? 'Please Enter Password' : null;
+                }),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Register'),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Register'),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) {
+                        return Login();
+                      }),
+                    );
+                  },
+                  child: const Text('Login'),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
